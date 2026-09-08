c408318184240f05a9242aa12ce3c153af5ca778
Hect0r420 <a.h.afsharinia@gmail.com>
2026-09-09 03:20:18 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
 import asyncio
 import logging
+import os  # این کتابخانه برای خواندن اطلاعات امنیتی است
 from aiogram import Bot, Dispatcher, F, types
 from aiogram.filters import Command
 from google import genai
