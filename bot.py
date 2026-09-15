46bcdf0bfcb6e45bbdbe85fa409b7cd5b736e4ce
Hect0r420 <a.h.afsharinia@gmail.com>
2026-09-15 04:01:08 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -308,11 +308,12 @@ async def handle_all_messages(message: types.Message):
 # ==========================================
 # ۴. استارت اصلی برنامه
 # ==========================================
+
 async def main():
     print(">>> ربات حرفه‌ای هکتور آنلاین شاپ با موفقیت روشن شد و آماده‌ی پاسخگویی است...")
 
     # راه‌اندازی دیتابیس
-await init_db()
+    await init_db()
     print(">>> دیتابیس راه‌اندازی شد.")
 
     RENDER_URL = os.getenv("RENDER_EXTERNAL_URL")
@@ -320,7 +321,7 @@ await init_db()
     WEBHOOK_PATH = f"/webhook/{TELEGRAM_BOT_TOKEN}"
 
     # ست کردن Webhook در تلگرام
-await bot.set_webhook(url=f"{RENDER_URL}{WEBHOOK_PATH}")
+    await bot.set_webhook(url=f"{RENDER_URL}{WEBHOOK_PATH}")
     print(f">>> Webhook تنظیم شد: {RENDER_URL}{WEBHOOK_PATH}")
 
     # ساخت وب‌سرور aiohttp
