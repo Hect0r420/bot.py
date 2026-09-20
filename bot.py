3dcd822b6a214993e2cd7a1a9876e6ce9d0daf26
Hect0r420 <a.h.afsharinia@gmail.com>
2026-09-21 01:54:25 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -2893,12 +2893,12 @@ async def main():
     RENDER_URL = os.getenv("RENDER_EXTERNAL_URL")
 PORT = int(os.getenv("PORT", 10000))
 
-if not RENDER_URL:
+    if not RENDER_URL:
     raise RuntimeError("RENDER_EXTERNAL_URL تنظیم نشده است")
 
-RENDER_URL = RENDER_URL.rstrip("/")
+    RENDER_URL = RENDER_URL.rstrip("/")
 
-WEBHOOK_PATH = f"/webhook/{TELEGRAM_BOT_TOKEN}"
+    WEBHOOK_PATH = f"/webhook/{TELEGRAM_BOT_TOKEN}"
 
 
     # ست کردن Webhook در تلگرام
